896a0196c5a00854745d0727cb5359a20352b569
Daiana Oliveira <daiana.davi2411@gmail.com>
2022-05-27 19:33:49 -0300

    Atividade 3 Lampada

---
+++ atividade3.js
@@ -0,0 +1,16 @@
+function changeSrc(id) {
+    var imagem=document.getElementById(id);
+    if(imagem.alt == "Abrir"){
+        imagem.src = "ligada.png";
+        imagem.alt = "Fechar";
+    }
+    else {
+        imagem.src = "apagada.png";
+        imagem.alt = "Abrir";
+    }
+}
+
+
+
+
+
